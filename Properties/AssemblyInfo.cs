744bb06b3aa76a29615348fa3591bf830df52ba4
sebastientromp <sebastien.tromp@gmail.com>
2023-01-31 14:38:18 +0100

    add download link

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -37,4 +37,4 @@ using IncreaseAnimationSpeedVsAI; // The namespace of your mod class
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: MelonInfo(typeof(IncreaseAnimationSpeedVsAIMod), "Increase Animation Speed vs AI", "0.0.2", "Sebastien Tromp")]
+[assembly: MelonInfo(typeof(IncreaseAnimationSpeedVsAIMod), "Increase Animation Speed vs AI", "0.0.3", "Sebastien Tromp", "https://github.com/sebastientromp/hs-melon-increase-game-speed-vs-ai")]
